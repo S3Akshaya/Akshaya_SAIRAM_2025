9df9f7be88aca9f2e2e98c452683945ae736b99f
S3Akshaya <126939340+S3Akshaya@users.noreply.github.com>
2024-01-21 18:06:09 +0530

    Update 21012024_python_division.py

--- 21012024_python_division.py
+++ 21012024_python_division.py
@@ -1,4 +1,4 @@
-Task
+''' Task
 The provided code stub reads two integers, a and b , from STDIN.
 
 Add logic to print two lines. The first line should contain the result of integer division, a //b . The second line should contain the result of float division, a /b .
@@ -17,7 +17,7 @@ Print:
 0.6
 Input Format
 
-''' The first line contains the first integer, a.
+The first line contains the first integer, a.
 The second line contains the second integer,b .
 
 Output Format
